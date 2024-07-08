0d8ee566b1546f72e7842818507c2cd98d6e0caa
Dev Chopra <94747317+DEV-D-GR8@users.noreply.github.com>
2024-07-08 06:27:13 +0530

    Update ContentView.swift

--- TextGenius/ContentView.swift
+++ TextGenius/ContentView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
-    @AppStorage("hasLaunchedBefore") var hasLaunchedBefore: Bool = false
+    @AppStorage("hasLaunchedBefore", store: UserDefaults(suiteName: "group.dev-d-gr8.TextGenius")) var hasLaunchedBefore: Bool = false
 
     var body: some View {
         if hasLaunchedBefore {
